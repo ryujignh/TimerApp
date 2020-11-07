8a67903c5db758b01b50e1c30729a0f35b6e6b4e
ryujignh <ryujignh@gmail.com>
2020-11-07 17:58:20 +0900

    Initial Commit

---
+++ TimerAppEN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerAppEN
+//
+//  Created by Ryuji Ganaha on 2020/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerAppEN/TimerAppENApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppENApp.swift
+//  TimerAppEN
+//
+//  Created by Ryuji Ganaha on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
